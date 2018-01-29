eccd491a7e2d762a9173d94fa8266a535d1a6568
Rick Riehle <rriehle@uw.edu>
2018-01-28 21:22:06 -0700

    Tune build script

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -3,8 +3,9 @@
 git checkout gh-pages
 rm -rf build/*
 git merge master
-make html
 touch .nojekyll  # Make sure the repo has this file in its root, otherwise it will not render on github.io
+make html
+open build/html/index.html
 git add *
 git commit -a -m "Updating presentation materials"
 git pull -s ours --no-edit
